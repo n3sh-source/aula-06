14e72817ca6b0eb951e2b211b88776b7fb9443d7
n3sh-source <gustavo.carakaki@gmail.com>
2022-03-18 22:01:22 -0300

    2 files modified, 7 added

---
+++ src/app/juros-compostos/juros-compostos.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juros-compostos',
+  templateUrl: './juros-compostos.component.html',
+  styleUrls: ['./juros-compostos.component.css'],
+})
+export class JurosCompostosComponent implements OnInit {
+  vp: number;
+  j: number;
+  n: number;
+  juros = [];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  getJurosCompostos() {
+    for (let i = 1; i <= this.n; i++) {
+      let simples = this.vp * (1 + (this.j / 100) * i);
+      let composto = Math.pow(this.vp * (1 + this.j / 100), i);
+      this.juros.push({ simples, composto, periodo: i });
+    }
+    return this.juros;
+  }
+}
